e0cd8161333f15770505fdc7f199838a7c7c87f4
jensengbg-sanel-jasarevic <55239936+jensengbg-sanel-jasarevic@users.noreply.github.com>
2020-03-27 22:58:33 +0100

    Reset game
    Added a reset function without, that does not require reloading the whole page

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
+// Global variables
 var numbers = [1, 2, 1, 2, 3, 4, 3, 4, 5, 6, 5, 6, 7, 8, 7, 8];
 var matches = 0;
 var pause = false;
 var clickedButtonId = undefined;
 var clickedButtonNumber = undefined;
 
+var playAgain = document.querySelector("#play-again");
 var buttons = document.querySelectorAll(".game-button");
 
 spreadNumbers();
+playAgain.addEventListener('click', reset);
+
 
 for (i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function (e) {
         var turnable = e.target.dataset.turnable;
 
-        //1st click
+        // 1st click
         if (!pause && clickedButtonId == undefined &&
              clickedButtonNumber == undefined && turnable == 'true') {
                 e.target.dataset.turnable = 'false';
@@ -24,7 +28,7 @@ for (i = 0; i < buttons.length; i++) {
                  clickedButtonNumber = e.target.dataset.number;
         }
 
-        //2nd click  
+        // 2nd click  
         if (!pause && clickedButtonId != undefined && clickedButtonNumber
             != undefined && turnable == 'true' &&
             e.target.id != clickedButtonId) {
@@ -32,7 +36,7 @@ for (i = 0; i < buttons.length; i++) {
 
                 e.target.textContent = e.target.dataset.number;
              
-                //Pair
+                //If pair
                 if (e.target.dataset.number == clickedButtonNumber) {
                     e.target.style.backgroundColor = 'green';
                     document.getElementById(clickedButtonId)
@@ -43,9 +47,12 @@ for (i = 0; i < buttons.length; i++) {
                     
                     matches++;
 
+                   // Reset matches to 0 when reaching 8/8
                     if (matches == 8) {
-                        alert ("Well done!");
+                        alert ("Game over!"); 
+                        matches = 0;
                     }
+                                            
 
                  //No pair   
                 } else {
@@ -75,6 +82,22 @@ for (i = 0; i < buttons.length; i++) {
     });
 }
 
+// Functions
+function reset() {
+    var numbers = [1, 2, 1, 2, 3, 4, 3, 4, 5, 6, 5, 6, 7, 8, 7, 8];
+    var pause = false;
+    var clickedButtonId = undefined;
+    var clickedButtonNumber = undefined;
+
+    spreadNumbers()
+    
+    for (i = 0; i < buttons.length; i++) {
+    buttons[i].dataset.turnable = 'true';
+    buttons[i].style.backgroundColor = 'indigo';
+    buttons[i].textContent = "";
+}
+}
+            
 function spreadNumbers() {
     for (i = 0; i < buttons.length; i++) {
         buttons[i].dataset.number = numbers[i];
